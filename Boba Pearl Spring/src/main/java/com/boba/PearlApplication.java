22eb0ebde1390f20c29b5061be9616719c157a34
David Jiang <djiang86@binghamton.edu>
2018-06-04 02:26:45 +0000

    starting template for boba pearl springboot

---
+++ Boba Pearl Spring/src/main/java/com/boba/PearlApplication.java
@@ -0,0 +1,12 @@
+package com.boba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PearlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PearlApplication.class, args);
+	}
+}
