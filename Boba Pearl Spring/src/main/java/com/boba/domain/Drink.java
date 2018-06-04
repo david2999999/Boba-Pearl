97e887487222a6653add3bdac2f6771741fd069e
David Jiang <djiang86@binghamton.edu>
2018-06-04 02:53:09 +0000

    added Drink.java class

--- Boba Pearl Spring/src/main/java/com/boba/domain/Drink.java
+++ Boba Pearl Spring/src/main/java/com/boba/domain/Drink.java
@@ -1,8 +1,38 @@
 package com.boba.domain;
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+import org.springframework.data.annotation.Id;
+
 import lombok.Data;
 
 @Data
 public class Drink {
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String description;
+	
+	@OneToOne
+	private Base base;
+	
+	@OneToOne
+	private Flavor flavor;
+	
+	@Enumerated(value = EnumType.STRING)
+	private Size size;
+	
+	@OneToMany(cascade = CascadeType.PERSIST, mappedBy = "drink")
+	private Set<Topping> toppings;
+	
 }
